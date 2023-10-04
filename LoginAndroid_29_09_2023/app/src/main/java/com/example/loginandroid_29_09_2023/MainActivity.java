64d05524181d2b224b35c01146a31343afcc3f10
Alberto Hernández Akkari <akkarihdez@gmail.com>
2023-10-04 10:45:00 +0200

    04-10-23 2

--- LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/MainActivity.java
+++ LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.loginandroid_29_09_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+    private void initComponents(){
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome To Miami!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
