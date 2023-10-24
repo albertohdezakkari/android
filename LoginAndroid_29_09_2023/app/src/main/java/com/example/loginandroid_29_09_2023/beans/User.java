84786ad430d4e3ab95c3b9de3981f7b315410174
Alberto Hernández Akkari <akkarihdez@gmail.com>
2023-10-24 10:50:35 +0200

    [Movies-003] - Retrofit Movies
    24-10-2023

---
+++ LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/beans/User.java
@@ -0,0 +1,19 @@
+package com.example.loginandroid_29_09_2023.beans;
+
+public class User {
+    private String email;
+    private String pass;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
